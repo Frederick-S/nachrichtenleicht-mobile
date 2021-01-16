7a1660e7480a4b0a530df2876839b913a3aa3cdc
Frederick-S <mao_xiaodan@hotmail.com>
2021-01-16 13:15:04 +0800

    Add news detail

--- lib/widget/news_detail.dart
+++ lib/widget/news_detail.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:nachrichtenleicht/model/news.dart';
+
+import '../error_reporter.dart';
 
 class NewsDetail extends StatefulWidget {
   final News news;
@@ -15,12 +21,21 @@ class NewsDetail extends StatefulWidget {
 
 class NewsDetailState extends State<NewsDetail> {
   News _news;
+  bool _isPlaying = false;
+  AudioPlayer _audioPlayer;
 
   @override
   void initState() {
     super.initState();
 
     this._news = widget.news;
+    _audioPlayer = AudioPlayer();
+
+    if (_hasAudio()) {
+      _audioPlayer.setUrl(_news.audioUrl).catchError((error, stackTrace) {
+        ErrorReporter.reportError(error, stackTrace);
+      });
+    }
   }
 
   @override
@@ -30,7 +45,92 @@ class NewsDetailState extends State<NewsDetail> {
           appBar: AppBar(
             title: Text('Nachrichtenleicht'),
           ),
+          body: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Stack(
+              children: <Widget>[_buildNewsBody()],
+            ),
+          ),
+          floatingActionButton: _hasAudio() ? _buildAudioPlayer() : Container(),
         ),
-        onWillPop: null);
+        onWillPop: () async {
+          if (_hasAudio()) {
+            try {
+              await _audioPlayer.dispose();
+            } catch (error, stackTrace) {
+              ErrorReporter.reportError(error, stackTrace);
+            }
+          }
+
+          return true;
+        });
+  }
+
+  Widget _buildNewsBody() {
+    return InAppWebView(
+      initialUrl: Uri.dataFromString(_buildNewsHtml(_news),
+              mimeType: 'text/html', encoding: utf8)
+          .toString(),
+      onConsoleMessage: (InAppWebViewController inAppWebViewController,
+          ConsoleMessage consoleMessage) {
+        print(consoleMessage.message);
+
+        if (consoleMessage.messageLevel == ConsoleMessageLevel.ERROR) {
+          ErrorReporter.reportError(consoleMessage.message, null);
+        }
+      },
+    );
+  }
+
+  _buildNewsHtml(News news) {
+    final image = news.imageUrl != '' ? '<img src=${news.imageUrl} />' : '';
+
+    return """
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        </head>
+        <body>
+          <style>
+            * {
+              font-size: 16px;
+            }
+            img {
+              max-width: 100%;
+            }
+          </style>
+          $image
+          <h1>${news.title}</h1>
+          ${news.body}
+        </body>
+      </html>
+    """;
+  }
+
+  bool _hasAudio() {
+    return _news.audioUrl != null && _news.audioUrl != '';
+  }
+
+  Widget _buildAudioPlayer() {
+    return FloatingActionButton(
+        onPressed: () async {
+          if (_audioPlayer.playbackState == AudioPlaybackState.stopped ||
+              _audioPlayer.playbackState == AudioPlaybackState.paused) {
+            _audioPlayer.play().catchError((error, stackTrace) {
+              ErrorReporter.reportError(error, stackTrace);
+            });
+          } else if (_audioPlayer.playbackState == AudioPlaybackState.playing) {
+            _audioPlayer.pause().catchError((error, stackTrace) {
+              ErrorReporter.reportError(error, stackTrace);
+            });
+          }
+
+          setState(() {
+            _isPlaying =
+                _audioPlayer.playbackState == AudioPlaybackState.playing;
+          });
+        },
+        child: Icon(_isPlaying ? Icons.pause : Icons.play_arrow));
   }
 }
